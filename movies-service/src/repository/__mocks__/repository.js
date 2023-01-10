0e36c6c1ad4ad8e48965611261d91855393f416f
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-10 19:44:22 -0300

    Desenvolvidos testes para rotas de movies

---
+++ movies-service/src/repository/__mocks__/repository.js
@@ -0,0 +1,39 @@
+const movies = [{
+    "_id": "63bc8ad72c04cb232b9aff95",
+    "titulo": "Os vingadores: Guerra Infinita",
+    "sinopse": "Os herois mais poderosos da Marvel enfrentando o Thanos",
+    "duracao": 120,
+    "dataLancamento": new Date('2023-01-01T00:00:00Z'),
+    "imagem": "https://www.luiztools.com.br/vingadores-eu.jpg",
+    "categorias": [
+        "Aventura",
+        "Ação"
+    ]
+}, {
+    "_id": "63bc8ad72c04cb232b9aff96",
+    "titulo": "Os Vingadores",
+    "sinopse": "Os Heróis mais poderosos da Marvel enfrentando o Loki",
+    "duracao": 100,
+    "dataLancamento": new Date('2023-01-01T00:00:00Z'),
+    "imagem": "https://www.luiztools.com.brvingadores.jpg",
+    "categorias": [
+        "Aventura",
+        "Ação"
+    ]
+}]
+
+async function getAllMovies() {
+    return movies;
+}
+
+async function getMovieById(id) {
+    movies[0]._id = id
+    return movies[0]
+}
+
+async function getMoviesPremieres() {
+    movies[0].dataLancamento = new Date()
+    return [movies[0]]
+}
+
+module.exports = { getAllMovies, getMovieById, getMoviesPremieres }
