884ada545680a431e345abe1e327035a19c1831d
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-10 20:18:17 -0300

    Desenvolvido ponto de inicio da aplicação

--- movies-service/src/index.js
+++ movies-service/src/index.js
@@ -1,1 +1,11 @@
-//index.js
+const movies = require('./api/movies');
+const repository = require('./repository/repository');
+const server = require('./server/server');
+
+(async () => {
+    try {
+        await server.start(movies, repository)
+    } catch (error) {
+        console.error(error);
+    }
+})()
