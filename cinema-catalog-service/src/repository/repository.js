1c6e9094e433968112c1a385c37ceb0fd76c736d
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-14 19:21:08 -0300

    Desenvolvida rotina para buscar os dados do cinema pelo id da  cidade

--- cinema-catalog-service/src/repository/repository.js
+++ cinema-catalog-service/src/repository/repository.js
@@ -3,7 +3,17 @@ const { ObjectId } = require('mongodb')
 
 async function getAllCities() {
     const db = await database.connect()
-    return db.collection('catalog').find({}).project({ cidade: 1, uf: 1, pais: 1 }).toArray()
+    return db.collection('catalog')
+        .find({})
+        .project({ cidade: 1, uf: 1, pais: 1 })
+        .toArray()
 }
 
-module.exports = { getAllCities } 
+async function getCinemasByCiyId(cityId) {
+    const objCotyId = new ObjectId(cityId)
+    const db = await database.connect()
+    return db.collection('catalog')
+        .findOne({ _id: objCotyId }, { projection: { cinemas: 1 } })
+}
+
+module.exports = { getAllCities, getCinemasByCiyId } 
--- cinema-catalog-service/src/repository/repository.test.js
+++ cinema-catalog-service/src/repository/repository.test.js
@@ -1,9 +1,21 @@
-const { test, expect } = require('@jest/globals')
+const { test, expect, beforeAll } = require('@jest/globals')
 const repository = require('./repository')
+
+let cityId = null
+
+beforeAll(async () => {
+    const cities = await repository.getAllCities()
+    cityId = cities[0]._id
+})
 
 test('getAllCities', async () => {
     const cities = await repository.getAllCities()
-    console.log(cities);
     expect(Array.isArray(cities)).toBeTruthy()
     expect(cities.length).toBeTruthy()
+})
+
+test('getCinemasByCiyId', async () => {
+    const city = await repository.getCinemasByCiyId(cityId)
+    expect(city).toBeTruthy()
+    expect(Array.isArray(city.cinemas)).toBeTruthy()
 })
