be28e4cf574fa5cd8726b56fe602eeb84ccef54a
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-19 20:03:03 -0300

    Desenvolvida classe de logger pro serviço de filmes

---
+++ movies-service/src/config/logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston')
+
+const logger = winston.createLogger({
+    format: winston.format.combine(
+        winston.format.errors({ stack: true }),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transport.File({
+            filrname: 'logs.txt',
+            level: 'error'
+        })
+    ]
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({ format: winston.format.simple() }))
+}
+
+module.exports = logger
