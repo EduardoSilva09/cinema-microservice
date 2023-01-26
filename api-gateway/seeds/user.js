595de548890bc5dfeb191fcb8d2ededfa6e36fce
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2023-01-26 18:31:45 -0300

    Adicionada seed de usuários

---
+++ api-gateway/seeds/user.js
@@ -0,0 +1,12 @@
+[
+    {
+        email: "eduardoadmin@email.com",
+        password: "",
+        profileId: 1
+    },
+    {
+        email: "eduardo@email.com",
+        password: "",
+        profileId: 2
+    }
+]
